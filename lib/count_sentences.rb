17dec50dbf44001f97688efc1606082220b9904a
Raymond1028 <lancelot53.rs@gmail.com>
2022-01-24 18:15:38 -0500

    commit tree eb5cedff7b98eb5ca07b93b3e766ee70269da673

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,12 +1,25 @@
 require 'pry'
 
 class String
+  def initalize
+
+  end
 
   def sentence?
+   if self.end_with?(".")
+    true
+   else 
+    false
+   end
     
   end
 
   def question?
+    if self.end_with?("!")
+      true
+     else 
+      false
+     end
 
   end
 
