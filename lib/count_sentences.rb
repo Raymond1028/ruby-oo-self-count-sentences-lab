948edd8892d9cebc81269bccaa8f6b70fb9aae68
Ray's Git <lancelot53.ra@gmail.com>
2022-01-26 16:09:03 -0500

    Completed

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,44 @@
-require 'pry'
+require "pry"
 
 class String
+  def initialize
+  end
 
   def sentence?
-    
+    if self.end_with?(".")
+      true
+    else
+      false
+    end
   end
 
   def question?
-
+    if self.end_with?("?")
+      true
+    else
+      false
+    end
   end
 
   def exclamation?
-
+    if self.end_with?("!")
+      true
+    else
+      false
+    end
   end
 
   def count_sentences
-
+    #self.reject { |c| c.empty? }
+     
+    self.split(/\.|\?|\!/).delete_if {|w| w.size < 2}.size
+    #binding.pry
+   # array_2 = array
+    #array = self.reject { |c| c.empty? }
+    #array.count
   end
+  
 end
+stringer = String.new
+stringer.count_sentences
+
